7f0a5d9e7fb1c823d2e4785afa6744f062adaa1e
Li Haoyi <haoyi@dropbox.com>
2014-04-20 22:52:53 -0700

    cross publishing against 2.10.4/2.11.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,50 @@
 import sbt._
 import Keys._
+import scala.scalajs.sbtplugin.ScalaJSPlugin.ScalaJSKeys._
+import scala.scalajs.sbtplugin.ScalaJSPlugin._
 object Build extends sbt.Build{
+  lazy val root = project.in(file("."))
+                         .aggregate(js, jvm)
+                         .settings(crossScalaVersions := Seq("2.10.4", "2.11.0"))
+
+  lazy val js = project.in(file("js"))
+                       .settings(sharedSettings ++ scalaJSSettings:_*)
+                       .settings(
+    libraryDependencies ++= Seq(
+      "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.3" % "provided",
+      "org.webjars" % "envjs" % "1.2" % "test",
+      "com.lihaoyi" %% "utest" % "0.1.3-JS" % "test",
+      "com.lihaoyi" %% "acyclic" % "0.1.2" % "provided"
+    ),
+    version := version.value + "-JS",
+    (loadedTestFrameworks in Test) := {
+      (loadedTestFrameworks in Test).value.updated(
+        sbt.TestFramework(classOf[utest.runner.JsFramework].getName),
+        new utest.runner.JsFramework(environment = (scalaJSEnvironment in Test).value)
+      )
+    }
+  )
+
+  lazy val jvm = project.in(file("jvm"))
+                        .settings(sharedSettings:_*)
+                        .settings(
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor" % "2.3.2" % "provided",
+      "com.lihaoyi" %% "utest" % "0.1.3" % "test",
+      "com.lihaoyi" %% "acyclic" % "0.1.2" % "provided"
+    ),
+    testFrameworks += new TestFramework("utest.runner.JvmFramework")
+  )
+
   val sharedSettings = Seq(
     organization := "com.scalarx",
     name := "scalarx",
-    scalaVersion := "2.10.3",
-
+    scalaVersion := "2.10.4",
+    version := "0.2.4",
+    unmanagedSourceDirectories in Compile <+= baseDirectory(_ / ".." / "shared" / "main" / "scala"),
+    unmanagedSourceDirectories in Test <+= baseDirectory(_ / ".." / "shared" / "test" / "scala"),
+    addCompilerPlugin("com.lihaoyi" %% "acyclic" % "0.1.2"),
+    autoCompilerPlugins := true,
     // Sonatype
     publishArtifact in Test := false,
     publishTo <<= version { (v: String) =>
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,8 @@
-addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.4.0")
+addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.4.3")
 
 resolvers += Resolver.sonatypeRepo("releases")
 
 resolvers += Resolver.sonatypeRepo("snapshots")
 
-addSbtPlugin("com.lihaoyi" % "utest-js-plugin" % "0.1.2")
+addSbtPlugin("com.lihaoyi" % "utest-js-plugin" % "0.1.3")
 
