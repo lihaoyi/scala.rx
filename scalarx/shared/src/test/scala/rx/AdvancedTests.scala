7e5dc54f3a73eba8b0af33c002ed37d5b8bccc03
Nick <nick@stabtechs.com>
2016-01-07 16:29:30 -0800

    0.3.0-SNAPSHOT

--- scalarx/shared/src/test/scala/rx/AdvancedTests.scala
+++ scalarx/shared/src/test/scala/rx/AdvancedTests.scala
@@ -6,7 +6,84 @@ import scala.util.{Success, Failure}
 
 import utest._
 object AdvancedTests extends TestSuite{
-  def tests = TestSuite{
+
+  //Using a static scope for some tests to check the more common use case of the Rx macro
+  object MacroDoesRightThing {
+    var top1Count = 0
+    var top2Count = 0
+    var inner1Count = 0
+    var inner2Count = 0
+
+    def chk() = {
+      assert(inner1Count == inner2Count)
+      assert(top1Count == top2Count)
+    }
+
+    val top = Var(List(1,2,3))
+    val other = Var(1)
+
+    def inner1(num: Int)(implicit topCtx: RxCtx) = Rx.build { newCtx =>
+      inner1Count += 1
+      other()(newCtx)
+    }(topCtx)
+
+    def inner2(num: Int)(implicit topCtx: RxCtx) = Rx {
+      inner2Count += 1
+      other()
+    }
+
+    val things1 = Rx {
+      top1Count += 1
+      top().map(a => inner1(a))
+    }
+
+    val things2 = Rx {
+      top2Count += 1
+      top().map(a => inner2(a))
+    }
+  }
+
+  object SafeTrait {
+    trait RxTrait {
+      implicit def ctx: RxCtx // leave ctx abstract
+      val aa = Var(1)
+      lazy val meh = Rx { aa() }
+    }
+
+    object RxObj extends RxTrait {
+      //override implicit val ctx = RxCtx.Unsafe
+      override implicit val ctx = RxCtx.safe()
+      val objRx = Rx { meh() }
+    }
+
+    class RxClass extends RxTrait {
+      //compile time error:
+      //override implicit val ctx = RxCtx.safe()
+      override implicit val ctx = RxCtx.Unsafe
+      val classRx = Rx { meh() }
+    }
+
+    //One other way RxCtx.safe() can be used
+    class RxClass2()(implicit ZZZ: RxCtx) extends RxTrait {
+      override val ctx = RxCtx.safe()
+    }
+
+    //Although this would be more normal
+    class RxClass3()(implicit override val ctx: RxCtx) extends RxTrait {
+    }
+  }
+
+  object TopLevelVarCombinators {
+    val aa = Var(1)
+
+    val mapped = aa.map(_ + 10)
+
+    val filtered = aa.filter(_ % 2 == 0)
+
+    val reduced = aa.reduce((a,b) => a+b)
+  }
+
+  def tests = TestSuite {
 //    'perf{
 //      'init{
 //        val start = System.currentTimeMillis()
@@ -31,6 +108,8 @@ object AdvancedTests extends TestSuite{
 //    }
     "nesting" - {
       "nestedRxs" - {
+        implicit val testctx = RxCtx.Unsafe
+
         val a = Var(1)
         val b = Rx{
           Rx{ a() } -> Rx{ math.random }
@@ -39,7 +118,37 @@ object AdvancedTests extends TestSuite{
         a() = 2
         assert(b.now._2.now == r)
       }
+      "macroDoesTheRightThing" - {
+        import MacroDoesRightThing._
+        chk()
+        other() = other.now + 1
+        top() = List(3,2,1)
+        top() = List(2,2,2)
+        top() = List(1,2,3)
+        other() = other.now + 1
+        chk()
+      }
+      "safeTrait" - {
+        import SafeTrait._
+        RxObj.aa() = 3
+        assert(RxObj.meh.now == 3)
+        assert(RxObj.objRx.now == 3)
+
+        val instance = new RxClass
+        assert(instance.meh.now == 1)
+        instance.aa() = 5
+        assert(instance.classRx.now == 5)
+
+        val instance2 = new RxClass2()(RxCtx.Unsafe)
+        assert(instance2.meh.now == 1)
+
+        val instance3 = new RxClass3()(RxCtx.Unsafe)
+        instance3.aa() = 42
+        assert(instance3.meh.now == 42)
+      }
       "recalc" - {
+        implicit val testctx = RxCtx.Unsafe
+
         var source = 0
         val a = Rx{
           source
@@ -57,6 +166,8 @@ object AdvancedTests extends TestSuite{
         assert(i == 2)
       }
       "multiset" - {
+        implicit val testctx = RxCtx.Unsafe
+
         val a = Var(1)
         val b = Var(1)
         val c = Var(1)
@@ -94,6 +205,8 @@ object AdvancedTests extends TestSuite{
         assert(i == 6)
       }
       "webPage" - {
+        implicit val testctx = RxCtx.Unsafe
+
         var fakeTime = 123
         trait WebPage{
           def fTime = fakeTime
@@ -115,7 +228,6 @@ object AdvancedTests extends TestSuite{
             case "www.mysite.com/about" => new AboutPage()
           }
         }
-
         assert(page.now.html.now == "Home Page! time: 123")
 
         fakeTime = 234
@@ -130,10 +242,9 @@ object AdvancedTests extends TestSuite{
         page.now.update()
         assert(page.now.html.now == "About Me, time: 456")
       }
-
     }
-
     "combinators" - {
+      implicit val testctx = RxCtx.Unsafe
       "foreach" - {
         val a = Var(1)
         var count = 0
@@ -155,7 +266,6 @@ object AdvancedTests extends TestSuite{
         assert(c.now == 2)
         assert(d.now == 6)
       }
-
       "mapAll" - {
         val a = Var(10L)
         val b = Rx{ 100 / a() }
@@ -173,7 +283,6 @@ object AdvancedTests extends TestSuite{
         assert(c.now == 1337)
         assert(d.toTry == Success("java.lang.ArithmeticException: / by zero"))
       }
-
       "filter" - {
         val a = Var(10)
         val b = a.filter(_ > 5)
@@ -186,14 +295,12 @@ object AdvancedTests extends TestSuite{
         a() = 19
         assert(b.now == 19)
       }
-
       "filterFirstFail" - {
         val a = Var(10)
         val b = a.filter(_ > 15)
         a() = 1
         assert(b.now == 10)
       }
-
       "filterAll" - {
         val a = Var(10L)
         val b = Rx{ 100 / a() }
@@ -207,7 +314,6 @@ object AdvancedTests extends TestSuite{
         a() = 1
         assert(c.now == 100)
       }
-
       "reduce" - {
         val a = Var(2)
         val b = a.reduce(_ * _)
@@ -220,7 +326,6 @@ object AdvancedTests extends TestSuite{
         a() = 4
         assert(b.now == 24)
       }
-
       "reduceAll" - {
         val a = Var(1L)
         val b = Rx{ 100 / a() }
@@ -245,7 +350,7 @@ object AdvancedTests extends TestSuite{
       }
 
       "killRx" - {
-        val (a, b, c, d, e, f) = Util.initGraph
+        val (a, b, c, d, e, f) = Util.initGraph()
 
         assert(c.now == 3)
         assert(e.now == 7)
@@ -276,7 +381,23 @@ object AdvancedTests extends TestSuite{
         assert(f.now == 36)
       }
     }
+    "topLevelCombinators" - {
+      import TopLevelVarCombinators._
+      assert(mapped.now == 11)
+      assert(filtered.now == 1)
+      assert(reduced.now == 1)
+      aa() = 2
+      assert(mapped.now == 12)
+      assert(filtered.now == 2)
+      assert(reduced.now == 3)
+      aa() = 3
+      assert(mapped.now == 13)
+      assert(filtered.now == 2)
+      assert(reduced.now == 6)
+    }
     "higherOrderRxs" - {
+      implicit val testctx = RxCtx.Unsafe
+
       val a = Var(1)
       val b = Var(2)
       val c = Rx(Rx(a() + b()) -> (a() - b()))
@@ -315,18 +436,16 @@ object AdvancedTests extends TestSuite{
       def y()(implicit zzz: RxCtx) = Rx { testY += 1; a() }
 
       //This way will leak an Rx (ie exponential blow up in cpu time), but is not caught at compile time
-      def z() = Rx { testZ += 1; a() }
+      def z() = Rx.unsafe { testZ += 1; a() }
 
-      //def y()(implicit qqq: RxCtx): Rx[Int] = Rx.build(implicit ctx => { test += 1; a()})
-      val yy = Rx{ a() ; for (i <- 0 until 100) yield y() }
-      val zz = Rx{ a() ; for (i <- 0 until 100) yield z() }
+      val yy = Rx.unsafe { a() ; for (i <- 0 until 100) yield y() }
+      val zz = Rx.unsafe { a() ; for (i <- 0 until 100) yield z() }
       a() = 1
       a() = 2
       a() = 3
       a() = 4
       assert(testY == 500)
       assert(testZ == 1500)
-
     }
   }
 }
--- scalarx/shared/src/test/scala/rx/Util.scala
+++ scalarx/shared/src/test/scala/rx/Util.scala
@@ -1,21 +1,20 @@
 package rx
 import acyclic.file
 
-
 object Util {
   /**
    * Generates a short dataflow graph for testing
    */
-  def initGraph = {
+  def initGraph()(implicit ctx: RxCtx) = {
     val a = Var(1) // 3
 
     val b = Var(2) // 2
 
-    val c = Rx{ a() + b() } // 5
+    val c = Rx { a() + b() } // 5
     val d = Rx{ c() * 5 } // 25
     val e = Rx{ c() + 4 } // 9
     val f = Rx{ d() + e() + 4 } // 25 + 9 + 4 =
 
-    (a, b, c, d, e, f)
+   (a, b, c, d, e, f)
   }
 }
