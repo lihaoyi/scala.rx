9944a2eec606cb3d92e92fc1b43e4674c541070a
johannes karoff <johannes@karoff.net>
2019-05-19 19:03:52 +0200

    performance: less allocations and inline functions

--- scalarx/shared/src/main/scala/rx/Var.scala
+++ scalarx/shared/src/main/scala/rx/Var.scala
@@ -1,12 +1,12 @@
 package rx
 
 import scala.util.{Success, Try}
-import scala.collection.mutable
+import scala.collection.{breakOut, mutable}
 
 trait Var[T] extends Rx[T] {
   def update(newValue: T): Unit
 
-  def update(f: T => T):Unit = {
+  @inline final def update(f: T => T):Unit = {
     update(f(now))
   }
 
@@ -14,9 +14,9 @@ trait Var[T] extends Rx[T] {
 
   private[rx] def depth: Int = 0
 
-  def toTry: Try[T] = util.Success(now)
+  @inline final def toTry: Try[T] = util.Success(now)
 
-  override def recalc(): Unit = propagate()
+  @inline final def recalc(): Unit = propagate()
 
   override def kill(): Unit = {
     clearDownstream()
@@ -40,9 +40,9 @@ object Var {
     */
   def set(args: Assignment[_]*): Unit = {
     args.foreach(_.set())
-    Rx.doRecalc(
-      args.flatMap(_.v.downStream),
-      args.flatMap(_.v.observers)
+    Rx.doRecalcMutable(
+      args.flatMap(_.v.downStream)(breakOut),
+      args.flatMap(_.v.observers)(breakOut)
     )
   }
 
@@ -57,11 +57,11 @@ object Var {
     * actually setting it.
     */
   object Assignment {
-    implicit def tuple2Assignment[T](t: (Var[T], T)): Assignment[T] = {
+    @inline implicit def tuple2Assignment[T](t: (Var[T], T)): Assignment[T] = {
       Assignment(t._1, t._2)
     }
 
-    implicit def tuples2Assignments[T](ts: Seq[(Var[T], T)]): Seq[Assignment[T]] = {
+    @inline implicit def tuples2Assignments[T](ts: Seq[(Var[T], T)]): Seq[Assignment[T]] = {
       ts.map(t => Assignment(t._1, t._2))
     }
   }
@@ -79,7 +79,7 @@ object Var {
 
     private[rx] var value = initialValue
 
-    override def now: T = value
+    @inline override final def now: T = value
 
     /**
       * Sets the value of this [[Var]] and runs any triggers/notifies
@@ -88,7 +88,7 @@ object Var {
     def update(newValue: T): Unit = {
       if (value != newValue) {
         value = newValue
-        Rx.doRecalc(downStream, observers)
+        Rx.doRecalcCopy(downStream, observers)
       }
     }
   }
@@ -98,7 +98,7 @@ object Var {
     protected val rx: Rx[S]
 
     // Proxy Rx
-    override def now: S = rx.now
+    @inline override final def now: S = rx.now
 
     override private[rx] def downStream = rx.downStream
     override private[rx] def observers = rx.observers
@@ -137,10 +137,16 @@ object Var {
     override def update(newValue: S): Unit = {
       if( value != newValue ) { // Assuming that read(write) == write(read) == identity
         value = newValue
+
         // avoid triggering rx, because we already
         // know the current value: newValue
-        Rx.doRecalc(
-          rx.downStream ++ (base.downStream - rx),
+
+        val newDownStream = mutable.PriorityQueue[Rx.Dynamic[_]]()
+        rx.downStream.foreach(newDownStream += _)
+        base.downStream.foreach(ds => if (ds != rx) newDownStream += ds)
+
+        Rx.doRecalcMutable(
+          newDownStream,
           rx.observers ++ base.observers
         )
       }
@@ -151,8 +157,11 @@ object Var {
     override private[rx] def value_=(newValue: S): Unit = {
       rx.cached = Success(newValue)
       base.value = write(newValue)
-      downStream ++= rx.downStream ++ (base.downStream - rx)
-      observers ++=  rx.observers ++ base.observers
+      downStream ++= rx.downStream
+      base.downStream.foreach(ds => if (ds != rx) downStream += ds)
+      observers ++= rx.observers
+      observers ++= base.observers
+      ()
     }
   }
 
@@ -174,8 +183,13 @@ object Var {
 
         // avoid triggering rx, because we already
         // know the current value: newValue
-        Rx.doRecalc(
-          rx.downStream ++ (base.downStream - rx),
+
+        val newDownStream = mutable.PriorityQueue[Rx.Dynamic[_]]()
+        rx.downStream.foreach(newDownStream += _)
+        base.downStream.foreach(ds => if (ds != rx) newDownStream += ds)
+
+        Rx.doRecalcMutable(
+          newDownStream,
           rx.observers ++ base.observers
         )
       }
@@ -186,8 +200,11 @@ object Var {
     override private[rx] def value_=(newValue: S): Unit = {
       rx.cached = Success(newValue)
       base.value = write(base.value, newValue)
-      downStream ++= rx.downStream ++ (base.downStream - rx)
-      observers ++=  rx.observers ++ base.observers
+      downStream ++= rx.downStream
+      base.downStream.foreach(ds => if (ds != rx) downStream += ds)
+      observers ++= rx.observers
+      observers ++= base.observers
+      ()
     }
   }
 
